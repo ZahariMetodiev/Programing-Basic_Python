1b5023d51754ff9c0232ea4782511b2b5561dbc9
ZahariMetodiev <zmetodiev4@gmail.com>
2023-04-28 12:16:42 +0300

    Add task Projects_Creation

---
+++ E01.First Steps in Coding/T07.Projects_Creation.py
@@ -0,0 +1,4 @@
+arhitect_name = input()
+number_of_project = int(input())
+
+print(f"The architect {arhitect_name} will need {number_of_project * 3} hours to complete {number_of_project} project/s.")
